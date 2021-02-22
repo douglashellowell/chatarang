1ab13bea670127626a337bdbf0ad3b1e1df33344
douglashellowell <doog99@gmail.com>
2021-02-22 12:57:41 +0000

    add list styling

--- components/List.js
+++ components/List.js
@@ -3,41 +3,47 @@ import { StyleSheet, View, Text, Button } from 'react-native';
 import SwipeGesture from './swipe-gesture';
 
 const List = (props) => {
-    console.log(props);
+  console.log(props);
 
-    const handleSwipe = (direction, index) => {
-        console.log(direction, index);
-        if(direction === "left"){
-            props.removeFromList(index);
-        }
-      }
-    return (
-        <View>
-               <Button title="add to list!" onPress={props.addToList}/>
-            {props.words.map((word, i) => {
-                return <View style={swipeStyles.textItem}>
-                    <SwipeGesture gestureStyle={swipeStyles.swipesGestureContainer} onSwipePerformed={handleSwipe} index={i}>
-                                  <Text>{word}</Text>
-                              </SwipeGesture>
-                </View>
-        })}
-        </View>
-    );
+  const handleSwipe = (direction, index) => {
+    console.log(direction, index);
+    if (direction === 'left') {
+      props.removeFromList(index);
+    }
+  };
+  return (
+    <View>
+      <Button title="add to list!" onPress={props.addToList} />
+      {props.words.map((word, i) => {
+        return (
+          <View style={swipeStyles.textItem}>
+            <SwipeGesture
+              gestureStyle={swipeStyles.swipesGestureContainer}
+              onSwipePerformed={handleSwipe}
+              index={i}
+            >
+              <Text>{word}</Text>
+            </SwipeGesture>
+          </View>
+        );
+      })}
+    </View>
+  );
 };
 
 const swipeStyles = StyleSheet.create({
-    container:{
-     height:'100%',
-     width:'100%'
-    },
-    swipesGestureContainer:{
-     height:'100%',
-     width:'100%',
-    },
-    textItem: {
-        borderColor: "#61dafb",
-        borderWidth: 4,
-    }
-   })
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  swipesGestureContainer: {
+    height: '100%',
+    width: '100%',
+  },
+  textItem: {
+    borderColor: '#61dafb',
+    borderWidth: 4,
+  },
+});
 
 export default List;
